13bb664f52cadb730e160e2d602f00d484c77727
Niall Thurrat <nt222fc@student.lnu.se>
2022-05-16 12:09:36 +0200

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    webextensions: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Service Worker Inspector
+
+A Chrome extension for protection against Service Worker security vulnerabilities
+by Niall Thurrat
