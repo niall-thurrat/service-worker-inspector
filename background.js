88f4c8c6b0fc4f0590c0dadac53bbb3f97839346
Niall Thurrat <nt222fc@student.lnu.se>
2022-06-09 11:35:52 +0200

    refactor

--- background.js
+++ background.js
@@ -56,46 +56,79 @@ function setNewStorageRequest (reqInitiator, reqUrl) {
 }
 
 chrome.runtime.onMessage.addListener(
-  async function (request, sender) {
+  function (request, sender) {
     if (request.type === 'scriptUrl') {
-      const url = (new URL(request.scriptUrl))
-      const params = new URLSearchParams(url.search)
-      const initiator = `${url.protocol}//${url.hostname}`
-
-      for (const param of params.values()) {
-        if (isValidHttpUrl(param)) {
-          const storageReqs = await getRequestsFromStorage(initiator)
-
-          if (storageReqs) {
-            storageReqs.urls.forEach(async (url) => {
-              const reqUrl = (new URL(url))
-              const reqDomain = `${reqUrl.protocol}//${reqUrl.hostname}`
-
-              if (param === reqDomain) {
-                const report = {
-                  checks: [{
-                    type: 'sw-xss',
-                    isPassing: false
-                  }]
-                }
-                // TODO: handle when report exists already
-                storageReqs.report = report
-
-                await chrome.storage.local.set({ [initiator]: storageReqs })
-
-                await chrome.storage.local.get(null, function (items) {
-                  console.log(`items: ${JSON.stringify(items)}`)
-                  console.log('----')
-                })
-              }
-            })
-          } // TODO: else... do I throw an error here if the initiator key not found???
-        }
-      }
+      const scriptUrl = (new URL(request.scriptUrl))
+      doSwxssCheck(scriptUrl)
     }
     return true
   }
 )
+
+async function doSwxssCheck (url) {
+  const initiator = `${url.protocol}//${url.hostname}`
+  const params = new URLSearchParams(url.search)
+  const paramUrls = getUrlsFromParams(params)
+
+  if (paramUrls) {
+    const storageReqs = await getRequestsFromStorage(initiator)
+
+    if (storageReqs) {
+      paramUrls.forEach(paramUrl => {
+        if (isMatchInStorage(storageReqs, paramUrl)) {
+          const report = createReport('sw-xss', false)
+          // TODO: handle if existing report with an addCheckToReport func
+          addReportToStorage(initiator, storageReqs, report)
+        }
+      })
+    } // TODO: else... handle if initiator key not found?
+  }
+
+  // DELETE - code for testing
+  await chrome.storage.local.get(null, function (items) {
+    console.log(`items: ${JSON.stringify(items)}`)
+    console.log('----')
+  })
+  // chrome.storage.local.clear()
+}
+
+function getUrlsFromParams (params) {
+  const paramUrls = []
+
+  for (const param of params.values()) {
+    if (isValidHttpUrl(param)) {
+      paramUrls.push(param)
+    }
+  }
+  return (paramUrls === []) ? null : paramUrls
+}
+
+async function isMatchInStorage (storageReqs, paramUrl) {
+  storageReqs.urls.forEach(async (url) => {
+    const reqUrl = (new URL(url))
+    // TODO: match more than just domains
+    const reqDomain = `${reqUrl.protocol}//${reqUrl.hostname}`
+
+    if (paramUrl === reqDomain) return true
+  })
+  return false
+}
+
+function createReport (type, result) {
+  // TODO: validation - type should be string, result should be boolean
+  const report = {
+    checks: [{
+      type,
+      isPassing: result
+    }]
+  }
+  return report
+}
+
+function addReportToStorage (initiator, storageReqs, report) {
+  storageReqs.report = report
+  chrome.storage.local.set({ [initiator]: storageReqs })
+}
 
 // credit goes to Pavlo on StackOverflow for this function
 // https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
