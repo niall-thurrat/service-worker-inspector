6ad11129f6e30706d2c31ca2b23105db4bd5235e
Niall Thurrat <nt222fc@student.lnu.se>
2022-06-09 12:18:26 +0200

    comments removed

--- swReadyScript.js
+++ swReadyScript.js
@@ -1,12 +1,10 @@
 (() => {
   if (!('serviceWorker' in navigator)) {
-    console.log('**** **** no service worker in navigator')
     return
   }
 
   navigator.serviceWorker.ready
     .then(swReg => {
-      console.log('**** **** **** **** THE SEVICEWORKER.READY FIRED!')
       const scriptURL = swReg.active.scriptURL
       document.dispatchEvent(new CustomEvent('swReadyEvent', { detail: scriptURL }))
     })
